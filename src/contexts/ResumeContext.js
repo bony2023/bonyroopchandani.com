3967ae9b02b33200b19c8291179ca25bf83f817c
Bony Roopchandani <bonyroopchandani@gmail.com>
2025-01-18 12:38:34 -0500

    update resume repo

--- src/contexts/ResumeContext.js
+++ src/contexts/ResumeContext.js
@@ -3,17 +3,18 @@ import React, { createContext, useState, useEffect } from "react"
 export const ResumeContext = createContext()
 
 export const ResumeContextProvider = (props) => {
+    const repo = "bony2023/resume-cdn"
     const [resumeUrl, setResumeUrl] = useState("")
 
     const constructResumeUrl = async (url) => {
         const sha = await fetch(url)
             .then(response => response.json())
             .then(data => data.sha)
-        setResumeUrl(`https://cdn.statically.io/gh/bony2023/resume/${sha}/resume.pdf`)
+        setResumeUrl(`https://cdn.statically.io/gh/${repo}/${sha}/resume.pdf`)
     }
 
     useEffect(() => {
-        constructResumeUrl('https://api.github.com/repos/bony2023/resume/commits/master')
+        constructResumeUrl(`https://api.github.com/repos/${repo}/commits/main`)
     }, [])
 
     return (
